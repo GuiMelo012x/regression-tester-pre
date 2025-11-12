c1c703a46ec6d03f53468b7a98ac6a13c11c0755
guilherme melo <guilherme.2020102296@unicap.br>
2025-11-11 23:02:38 -0300

    Adição do Cohere - Adicionado o cohere em sua versão 'command-a-03-2025', que é o modelo com melhor desempenho e justo (que não é flash) que a Cohere oferece.

--- reg.py
+++ reg.py
@@ -1,15 +1,44 @@
 import google.generativeai as genai # pip install google-generativeai
+import cohere # pip install cohere
+import os
+from dotenv import load_dotenv # pip install python-dotenv
 
-api_key = "AIzaSyBA5S_3VZRCkOmdzXFN3nMY-15_hHADQ6I"
-genai.configure(api_key=api_key)
+load_dotenv() # Carrega variáveis de ambiente do arquivo .env"
+google_api_key = os.getenv("GOOGLE_API_KEY") # Pega a chave do Google
+cohere_api_key = os.getenv("COHERE_API_KEY") # Pega a chave do Cohere
 
-print("Conectando com o Gemini")
+if not google_api_key or not cohere_api_key:
+    print("Erro: Chaves de API não encontradas no arquivo .env,")
+    print("Verifique se as keys estão no seu .env")
+    exit() # Para o script se não achar as chaves
 
-model = genai.GenerativeModel("gemini-2.5-pro")
+genai.configure(api_key=google_api_key) # Configura a chave da API do Google
+co_client = cohere.Client(cohere_api_key) # Configura a chave da API do Cohere
 
-response = model.generate_content("Qual a capital de Pernambuco?") # Teste
+# --- Pergunta de Teste ---
+pergunta = "Qual a capital de Pernambuco?"
+print(f"Pergunta: {pergunta}\n")
 
-print("Conexão realizada")
+    # ---------- Testando o Gemini ----------
+try:
+    print("---------- Gemini ----------")
+    print("Conectando com o Gemini...")
+    model_gemini = genai.GenerativeModel("gemini-2.5-pro")
+    response_gemini = model_gemini.generate_content(pergunta)
+    print("Resposta do Gemini:", response_gemini.text)
+except Exception as e:
+    print("Erro ao conectar com o Gemini:", str(e))
 
-print("\nResposta do Gemini:")
-print(response.text)
+
+     # ---------- Testando o Cohere ----------
+print("\n---------- Cohere ----------")
+
+try:
+    print("Conectando com o Cohere...")
+    response_cohere = co_client.chat (
+        model='command-a-03-2025',
+        message=pergunta
+    )
+    print("Resposta do Cohere:", response_cohere.text)
+except Exception as e:
+    print("Erro ao conectar com o Cohere:", str(e))
