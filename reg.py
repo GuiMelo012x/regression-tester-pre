1db20f29eb52a5b9b5e418f0a14f06e62771a600
guilherme melo <guilherme.2020102296@unicap.br>
2026-05-09 15:59:01 -0300

    Mudança no tempo de pausa - A mudança foi necessária pois o arquivo CSV com os resultados anteriores foi apagado, pois o Gemini deu erro de muitas requisições, por isso, acabou sendo prejudicado. - O programa será rodado novamente, agora com uma pausa de 1 minuto para garantir que o Gemini vai conseguir rodar tudo corretamente. - O programa dura cerca de 30 minutos para rodar e gerar o arquivo CSV novo.

--- reg.py
+++ reg.py
@@ -114,8 +114,8 @@ for filepath in ficheiros:
         "Llama3": resp_hf
     })
     # A pausa obrigatória para respeitar a Free Tier, pois estava bloqueando por "muitas requisições".
-    print("Aguardando 30 segundos para evitar bloqueios de API...") 
-    time.sleep(30)
+    print("Aguardando 60 segundos para evitar bloqueios de API...") 
+    time.sleep(60)
 
 # --- FIM DO LOOP ---
 
